2fbb3401ad49da3415f23065e12fe3cb8f4f16c0
gabriel z <gabizpiccinini@gmail.com>
2022-10-31 12:42:58 -0300

    adding other two new projects

---
+++ Dice Roll Game/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace RollTheDice
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            dynamic playerRandomNum=0, aiRandomNum=0;
+            int playerScore=0,aiScore=0;
+            
+            Random random = new Random();
+            
+            Console.WriteLine("ROLL THE DICE GAME!");
+            Thread.Sleep(500);
+            
+            Console.WriteLine("___________________");
+            Thread.Sleep(1000);
+            
+            Console.WriteLine("\nBest of 5!");
+            
+            while(playerScore!=5 && aiScore!=5)
+            {
+                Console.WriteLine("\nPress any key to roll the dice");
+                Console.ReadKey();
+                
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine($"You rolled a {playerRandomNum}!");
+                Thread.Sleep(1000);
+
+                aiRandomNum = random.Next(1, 7);
+                Console.WriteLine($"\nAI rolled a {aiRandomNum}");
+
+                if(playerRandomNum>aiRandomNum)
+                {
+                    Console.WriteLine("You won the round!");
+                    playerScore++;
+                }
+                else if(aiRandomNum>playerRandomNum)
+                {
+                    Console.WriteLine("You lost the round");
+                    aiScore++;
+                }
+                else if(aiRandomNum==playerRandomNum)
+                {
+                    Console.WriteLine("It's a draw!");
+                }
+
+            }
+            if(aiScore>playerScore)
+            {
+                Console.WriteLine("You lost.");
+            }
+            else
+            {
+                Console.WriteLine("You won!");
+            }
+            Console.WriteLine("Scores");
+            Console.WriteLine($"AI score: {aiScore}. Your score: {playerScore}.");
+            Console.ReadKey();
+        }
+    }
+}
+    
---
+++ Project Price Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace teste
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing the funcion to calculate the full price of 5 done projects");
+            PriceCalc();
+        }
+        static void PriceCalc()
+        {
+            double[] price = new double[5];
+            double[] individualPrice = new double[5];
+            double valor = 0;
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine($"Enter project price {i+1}");
+                price[i] = double.Parse(Console.ReadLine());
+                valor = valor + price[i];
+            }
+            System.Console.WriteLine("\nProjects list:");
+            for(int z=0;z<5;z++)
+            {
+                System.Console.WriteLine($"{z+1}. Price: ${price[z]}");
+            }
+            double finalPrice = valor-(valor*0.18);
+            Console.WriteLine($"\nThe total amount of the projects is {finalPrice}");
+            Console.WriteLine($"Insert the number of the speficic project you want to know the price");
+            int a = int.Parse(Console.ReadLine());
+            individualPrice[a] = price[a]*0.82;
+            Console.WriteLine($"The price of the projet {a} is {individualPrice[a]}");
+            Console.ReadKey();
+        }
+    }
+}
